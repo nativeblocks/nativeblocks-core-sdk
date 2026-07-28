1ad471bb44f5b0064871211e4caa602f3742625b
Alex <alex@nativeblocks.io>
2026-07-28 22:38:56 +0300

    feat: drop all props from root block to make it pure

--- platforms/ios/runtime/Sources/NativeblocksRuntime/api/provider/block/defaults/RootBlock.swift
+++ platforms/ios/runtime/Sources/NativeblocksRuntime/api/provider/block/defaults/RootBlock.swift
@@ -6,50 +6,13 @@ internal struct RootBlock: View {
     @Environment(\.nativeWindowWidthClass) var windowManager
 
     var body: some View {
-        let properties = blockProps.block.properties
         let slots = blockProps.block.slots
-
-        let paddingStart = findWindowSizeClass(properties["paddingStart"], windowManager)?.toCGFloat() ?? 0.0
-        let paddingTop = findWindowSizeClass(properties["paddingTop"], windowManager)?.toCGFloat() ?? 0.0
-        let paddingEnd = findWindowSizeClass(properties["paddingEnd"], windowManager)?.toCGFloat() ?? 0.0
-        let paddingBottom = findWindowSizeClass(properties["paddingBottom"], windowManager)?.toCGFloat() ?? 0.0
-
-        let alignmentHorizontal = findWindowSizeClass(properties["alignmentHorizontal"], windowManager) ?? "leading"
-        let spacing = (findWindowSizeClass(properties["spacing"], windowManager) ?? "").toCGFloat() ?? 0
-
-        let backgroundColor = findWindowSizeClass(properties["backgroundColor"], windowManager) ?? "#FFFFFFFF"
         let contentSlot = slots["content"]
 
-        VStack(
-            alignment: findAlignmentHorizontal(alignmentHorizontal),
-            spacing: spacing
-        ) {
+        VStack {
             if let contentSlot {
                 blockProps.onSubBlock(blockProps.block.subBlocks ?? [:], contentSlot, NONE_INDEX, nil)
             }
         }
-        .padding(EdgeInsets(top: paddingTop, leading: paddingStart, bottom: paddingBottom, trailing: paddingEnd))
-        .background(Color(hex: backgroundColor))
-    }
-}
-
-private func findAlignmentHorizontal(_ alignment: String) -> HorizontalAlignment {
-    switch alignment.lowercased() {
-    case "leading": return .leading
-    case "trailing": return .trailing
-    case "listrowseparatorleading":
-        if #available(iOS 16.0, *) {
-            return .listRowSeparatorLeading
-        } else {
-            return .leading
-        }
-    case "listrowseparatortrailing":
-        if #available(iOS 16.0, *) {
-            return .listRowSeparatorTrailing
-        } else {
-            return .leading
-        }
-    case "center": return .center
-    default: return .leading
     }
 }
